5297928d01feb97698818bd50b36be2e06d25669
Otavio Remedio <otavioremedio@gmail.com>
2017-05-03 14:34:34 -0300

    aula15

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   #define root, se chamar no nome do site irá para esse controller e pagina
   root 'demo#index'
   get 'demo/index'
+  get 'demo/hello'
+  get 'demo/other_hello'
+  get 'demo/lynda'
   #default route
   #may go away in future versions of Rails
   #get ':controller(/:action(/:id))'
